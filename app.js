bdbc905f4f989e3cd4aa2ea8f1c5e1ab736a946a
Vanel Kamdoum <ditrix.360@gmail.com>
2022-05-29 19:54:53 -0400

    Reformating app.js

--- app.js
+++ app.js
@@ -2,31 +2,59 @@ const { spawn } = require("child_process");
 const path = require("path");
 const player = require('play-sound')();
 
-const alexa_app_worker = spawn("/usr/bin/bash", [path.join(__dirname, "scripts", "launchsampleapp.sh")]);
-const wakeworker = spawn('/usr/bin/python', [ "-u", path.join(__dirname, 'wakeworker.py'), "--daemonq"]);
+const AlexaStates = {
+    UNKNOWN: 'UNKNOWN',
+    CONNECTING: 'CONNECTING',
+    CONNECTED: 'CONNECTED',
+    IDLE: 'IDLE',
+    LISTENING: 'LISTENING',
+    THINKING: 'THINKING',
+    SPEAKING: 'SPEAKING'
+}
+const connecting_phrase = "Connecting...";
+const connected_phrase = "Successfully registered";
 const idle_phrase = "Alexa is currently idle!";
-wakeworker.on("close", (code)=>{
+const listening_phrase = "Listening...";
+const thinking_phrase = "Thinking...";
+const speaking_phrase = "Speaking...";
+
+var currentState = AlexaStates.UNKNOWN;
+
+const alexa_app_worker = spawn("/usr/bin/bash", [path.join(__dirname, "scripts", "launchsampleapp.sh")]);
+const wakeworker = spawn('/usr/bin/python', ["-u", path.join(__dirname, 'wakeworker.py')]);
+
+wakeworker.on("close", (code) => {
     throw new Error(`wake worker process exited with code ${code}`);
 })
-var is_idle = false,
-is_first_time = true;
-alexa_app_worker.stdout.on('data', (data)=>{
-    if (data.toString().includes(idle_phrase)){
-        if(is_first_time){
-            player.play(path.join(__dirname,"assets", "hello.mp3"));
-            is_first_time = false;
-        }
-        is_idle = true;
-    }
-})
-wakeworker.stdout.on('data', (data) => {
-    if (is_idle){
-        is_idle = false;
-        player.play(path.join(__dirname,"assets", "alexayes.mp3"), ()=>{
-            alexa_app_worker.stdin.write("t\n")
-        });
-    }
-});
-wakeworker.stderr.on('data', (data)=>{
+
+alexa_app_worker.stdout.on('data', HandleStateChange);
+
+wakeworker.stdout.on('data', HandleWakeWordActivation);
+wakeworker.stderr.on('data', (data) => {
     throw new Error(`Wake worker error: ${data}`);
 })
+
+// TODO: Have to think about stopping an ongoing interaction
+function HandleWakeWordActivation() {
+    if (currentState !== AlexaStates.LISTENING && currentState !== AlexaStates.CONNECTING) {
+        alexa_app_worker.stdin.write("t\n");
+    }
+}
+
+function HandleStateChange(data) {
+    const string_data = data.toString();
+    if (string_data.includes(connecting_phrase)) {
+        currentState = AlexaStates.CONNECTING;
+    } else if (string_data.includes(connected_phrase)) {
+        player.play(path.join(__dirname, "assets", "hello.mp3"));
+        currentState = AlexaStates.CONNECTED;
+    } else if (string_data.includes(idle_phrase)) {
+        currentState = AlexaStates.IDLE;
+    } else if (string_data.includes(listening_phrase)) {
+        currentState = AlexaStates.LISTENING;
+    } else if (string_data.includes(thinking_phrase)) {
+        currentState = AlexaStates.THINKING;
+    } else if (string_data.includes(speaking_phrase)) {
+        currentState = AlexaStates.SPEAKING;
+    }
+}
